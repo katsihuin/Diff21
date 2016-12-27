f538f2da15f3f230ea8700e352978fbfaefac90e
Katherin Sihuin <katsihuin@gmail.com>
2016-12-27 11:28:15 -0500

    main.js agregado

--- main.js
+++ main.js
@@ -1,72 +1,28 @@
-
-(function(){
-
-    var formulario = document.getElementByName("formulario")[0]
-        elementos = formulario.elements,
-        boton = document.getElementById("btn");
-
-    var validarMonkeyA = function(e){
-        if (formulario.monkeyA.checked == true ){
-        } else {
-            alert ("Completa el campo Monkey");
-            e.preventDefault();
-        }
-    };
-        var validarMonkeyB = function(e){
-        if (formulario.monkeyB.checked == true ){
-        } else {
-            alert ("Completa el campo Monkey");
-            e.preventDefault();
-        }
-    };
-    var validarSmile = function(e){
-        if (formulario.smile[0].checked == true || formulario.smile[1].checked == false){
-            img.innerHTML = '<img src="img/monkeyHappy.jpg">';
-        } else {
-            img.innerHTML = '<img src="img/monkeySad.jpg">';
-            alert("Completa el campo Smile");
-            e.preventDefault();
-        }
-    }
-
-
-    var validar = function(e){
-        validarMonkeyA(e);
-        validarMonkeyB(e);
-        validarSmile(e);
-    };
-
-    formulario.addEventListener("submit", validar);
-
-}())
-var numero1 = document.getElementById("numero1");
-var diferencia = 0;
-
 function calcular()
 {
-	var  numero1=numero1.value;
-	var valorNumerico=parseFloat(numero1);
+	var  numberEntered = document.getElementById('numberEntered').value;
+	var numberValue=parseFloat(numberEntered);
 	
-	if(numero1 == "" )
+	if(numberEntered == "" )
 	{
 		salida.innerHTML = "--";
-		document.getElementById("").innerHTML
+		document.getElementById("salida").innerHTML
 	}
-	else if (val_num1 > 21 )
+	else if (numberValue < 21 )
 	{
-		diferencia =(( parseFloat(numero1.value)-21)*2);
-		var valorDiferencia= parseFloat(diferencia);
-		salida.innerHTML = valorDiferencia.toFixed(2);
-		document.getElementById("").innerHTML 
+		diff = 21- parseFloat(numberEntered.value);
+        var valueDiff = parseFloat(diferencia);
+        salida.innerHTML = valueDiff.toFixed(2);
+        document.getElementById("salida").innerHTML
 	
 	}
 	else 
-	{
-		diferencia =21- parseFloat(numero1.value);
-		var valorDiferencia= parseFloat(diferencia);
-		salida.innerHTML = valorDiferencia.toFixed(2);
-		document.getElementById("").innerHTML
-	}
+	{ 
+	   diff = ((parseFloat(numberEntered.value)-21)*2);
+        var valueDiff = parseFloat(diferencia);
+        salida.innerHTML = valueDiff.toFixed(2);
+        document.getElementById("salida").innerHTML
+    }
 
 }
   
